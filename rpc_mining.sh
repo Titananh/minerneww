a7257552fd108df4780f40861bbf51720b2f5d8e
Titananh <91409544+Titananh@users.noreply.github.com>
2025-08-26 11:21:45 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w deroi1qyrt547ydu5xl2wq2jpct9wv2ujeapurczv66rdh39w70n3m82kfuq9pvfz92xcqqqqe3fpsdwgqw8x68y -r sg1.fastpool.xyz -p rpc;
+    ./astrominer -w deroi1qyrt547ydu5xl2wq2jpct9wv2ujeapurczv66rdh39w70n3m82kfuq9pvfz92xcqqqqe3fpsdwgqw8x68y -r sg1.fastpool.xyz:10100 -p rpc;
     sleep 5;
 done
